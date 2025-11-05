fdf3b878902431d9e70bb384b5534d95bc38ce49
bhanu <shizzuuuuu@gmail.com>
2025-11-05 09:44:19 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from "express";
+import serverless from "serverless-http";
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Vercel API!" });
+  res.json({ message: "Hello from Express on Vercel!" });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
-
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Vercel Serverless Function!" });
-}
+export default serverless(app);
